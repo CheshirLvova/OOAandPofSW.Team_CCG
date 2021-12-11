f107b2def5de53639463eccb93987eb40e3aa965
NazarHR <47197105+NazarHR@users.noreply.github.com>
2021-12-11 10:20:47 +0200

    DBConnection
    -created entities for database
    -added databse connection

---
+++ code/ToEmit.Domain/Entities/Scores.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToEmit.Domain
+{
+    public partial class Scores
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Score { get; set; }
+
+        public virtual Users UsernameNavigation { get; set; }
+    }
+}
---
+++ code/ToEmit.Domain/Entities/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToEmit.Domain
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string EmailAddres { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
+
+        public virtual Scores Scores { get; set; }
+    }
+}
