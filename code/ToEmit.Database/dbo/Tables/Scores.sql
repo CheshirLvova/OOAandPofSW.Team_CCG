8eb364734e811284fbe57e97cc629438035f0b7d
NazarHR <47197105+NazarHR@users.noreply.github.com>
2021-12-11 09:50:51 +0200

    Storage
    Created database

---
+++ code/ToEmit.Database/dbo/Tables/Scores.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Scores]
+(
+	[Id] INT NOT NULL IDENTITY , 
+    [Username] NCHAR(50) NOT NULL UNIQUE, 
+    [Score] INT NOT NULL, 
+    CONSTRAINT [FK_Scores_Users] FOREIGN KEY ([Username]) REFERENCES [Users]([Username]), 
+    PRIMARY KEY ([Id])
+)
---
+++ code/ToEmit.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToEmit.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
